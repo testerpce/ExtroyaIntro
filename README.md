ed6157e31a340ed97345839efdcf5e7cfb43f41c
testerpce <sayambhuthesaviour@gmail.com>
2018-04-06 19:06:40 +0530

    First

--- README.md
+++ README.md
@@ -2,3 +2,40 @@
 NLU project
 
 Let it Go! Let it goooo!
+
+Files in this repository(Update as it is put in)
+1)preprocess.py (Contains the preprocessed text data and the batches for training, validation and test sets
+
+
+
+
+I am adding the preprocessing file for now.
+
+
+#One improvement all of us should try to implement is using 4 separate  two class classifiers.
+#Since each label has a label of 4 tags of which each tag can be one of two entities so 
+#Each of 4 classifiers will try to predict one entity for each tag  e.g. for _,_,_,_ each position is a tag and here the first tag
+#takes values 'E' or 'N'
+
+I will upload the preprocessing file later where there will be four binary columns indicating one of the two binary entities for each of four tags.
+
+
+Aniket will try to implement the RNN model in the kernel for that facebook data.
+#The improvement point is using biRNNs. Try to do the same code using biRNNs
+
+https://www.kaggle.com/prnvk05/rnn-mbti-predictor/notebook
+
+I will try to implement the Bag of words based classifier according to this kernel
+#I know there is no improvement on using these methods which have tfidf or bag of words.
+# But mostly I will improve preprocessing as much as possible. You two should not focus on preprocessing. I will do that.
+
+https://www.kaggle.com/depture/multiclass-and-multi-output-classification
+
+Rishi will try to implement the CNN based classifier according to this code on github or his own code if he wants
+#The improvement part might be to get multilayer CNNs or different pooling methods
+
+https://github.com/aboyker/convnet-document-classification
+
+
+
+
---
+++ preprocess.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 18:35:54 2018
+
+@author: sayambhu
+"""
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import pickle
+
+#The dataset . Make sure that the data set is in the same folder as this file
+text=pd.read_csv("mbti_1.csv" ,index_col='type')
+
+#Making labels one hot
+from sklearn.preprocessing import LabelBinarizer
+
+labels=text.index.tolist()
+encoder=LabelBinarizer(neg_label=0, pos_label=1, sparse_output=False)
+labels=encoder.fit_transform(labels)
+labels=np.array(labels)
+
+#Preprocessing data
+
+import re
+
+# Function to clean data ... will be useful later
+def post_cleaner(post):
+    """cleans individual posts`.
+    Args:
+        post-string
+    Returns:
+         cleaned up post`.
+    """
+    # Covert all uppercase characters to lower case
+    post = post.lower() 
+    
+    # Remove |||
+    post=post.replace('|||',"") 
+
+    # Remove URLs, links etc
+    post = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', '', post, flags=re.MULTILINE) 
+    # This would have removed most of the links but probably not all 
+
+    # Remove puntuations 
+    puncs1=['@','#','$','%','^','&','*','(',')','-','_','+','=','{','}','[',']','|','\\','"',"'",';',':','<','>','/']
+    for punc in puncs1:
+        post=post.replace(punc,'') 
+
+    puncs2=[',','.','?','!','\n']
+    for punc in puncs2:
+        post=post.replace(punc,' ') 
+    # Remove extra white spaces
+    post=re.sub( '\s+', ' ', post ).strip()
+    return post
+
+# Clean up posts
+# Covert pandas dataframe object to list. I prefer using lists for prepocessing. 
+posts=text.posts.tolist()
+posts=[post_cleaner(post) for post in posts]
+
+# Count total words
+from collections import Counter
+
+word_count=Counter()
+for post in posts:
+    word_count.update(post.split(" "))
+    
+vocab_len=len(word_count)
+
+#Making words a sintegers
+# Create a look up table 
+vocab = sorted(word_count, key=word_count.get, reverse=True)
+# Create your dictionary that maps vocab words to integers here
+vocab_to_int = {word: ii for ii, word in enumerate(vocab, 1)}
+
+posts_ints=[]
+for post in posts:
+    posts_ints.append([vocab_to_int[word] for word in post.split()])
+
+#making posts uniform
+    
+posts_lens = Counter([len(x) for x in posts])
+print("Zero-length reviews: {}".format(posts_lens[0]))
+print("Maximum review length: {}".format(max(posts_lens)))
+print("Minimum review length: {}".format(min(posts_lens)))
+
+seq_len = 500
+features=np.zeros((len(posts_ints),seq_len),dtype=int)
+for i, row in enumerate(posts_ints):
+    features[i, -len(row):] = np.array(row)[:seq_len]
+print(features[:10])
+
+
+split_frac = 0.8
+
+num_ele=int(split_frac*len(features))
+rem_ele=len(features)-num_ele
+train_x, val_x = features[:num_ele],features[num_ele:int(rem_ele/2)+num_ele]
+train_y, val_y = labels[:num_ele],labels[num_ele:int(rem_ele/2)+num_ele]
+
+test_x =features[num_ele+int(rem_ele/2):]
+test_y = labels[num_ele+int(rem_ele/2):]
+
+print("\t\t\tFeature Shapes:")
+print("Train set: \t\t{}".format(train_x.shape), 
+      "\nValidation set: \t{}".format(val_x.shape),
+      "\nTest set: \t\t{}".format(test_x.shape))
+
